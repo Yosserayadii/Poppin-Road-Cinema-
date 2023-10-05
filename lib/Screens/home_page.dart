c4f83c4e8b16e8b039a9150e14dda2443755d27e
YosserAyadii <yosserayadiiii@gmail.com>
2023-10-05 15:09:00 +0100

    base project

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:poppinroadcimema/reusable_widgets/Bottom_navigation_bar.dart';
+import 'package:poppinroadcimema/reusable_widgets/Custom_button.dart';
 
 class homescreen extends StatefulWidget {
   const homescreen({super.key});
@@ -10,8 +12,20 @@ class homescreen extends StatefulWidget {
 class _homescreenState extends State<homescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('home page'),
+    return Scaffold(
+      body: Container(
+        child: Container(
+          child: CustomButton(
+            title: 'My Button',
+            color: Color(0xFFF70113), 
+            onPressed: () {
+             
+            },
+
+          ),
+        ),
+      ),
+     bottomNavigationBar: Bottom_navigation_bar(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poppinroadcimema/Routes/Route.dart';
 import 'package:poppinroadcimema/Screens/splash.dart';
 
 void main() {
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Splash()
+      initialRoute: '/', 
+     routes: routes,
     );
   }
 }
