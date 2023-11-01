9219efc573af12da5102893d98fb3767d6fa2be3
YosserAyadii <yosserayadiiii@gmail.com>
2023-11-01 21:07:14 +0100

    confetti

--- lib/animations/splash.dart
+++ lib/animations/splash.dart
@@ -31,7 +31,7 @@ class _SplashState extends State<Splash> {
       Navigator.of(context).pushReplacement(
         PageRouteBuilder(
           pageBuilder: (context, animation, secondaryAnimation) =>
-              homescreen(),
+              Spinning_wheel(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             const begin = Offset(0.0, 1.0);
             const end = Offset.zero;
