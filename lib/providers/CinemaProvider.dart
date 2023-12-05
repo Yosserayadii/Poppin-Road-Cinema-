034a29c0ee3dc99e599ef4978af8eff159435167
yosra-wan <yosrawanene28@gmail.com>
2023-12-05 23:11:04 +0100

    test

--- lib/providers/CinemaProvider.dart
+++ lib/providers/CinemaProvider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:poppinroadcimema/Models/Cinema.dart';
+import 'package:poppinroadcimema/Models/Movie.dart';
 
 class CinemaProvider with ChangeNotifier {
   late DatabaseReference _databaseReference;
@@ -16,8 +17,33 @@ class CinemaProvider with ChangeNotifier {
     DataSnapshot snapshot = await _databaseReference.child('cinemas').get();
     if (snapshot.value != null && snapshot.value is List) {
       final cinemasDataList = snapshot.value as List<dynamic>;
-      cinemasData = cinemasDataList.map((CinemaProvider) {
-        final cinemaMap = Map<String, dynamic>.from(CinemaProvider);
+      List<Cinema> tempCinemasData = cinemasDataList.map((cinemaData) {
+        final cinemaMap = Map<String, dynamic>.from(cinemaData);
+
+        // Fetch and map movies data for each cinema
+        List<Movie>? moviesList = [];
+        if (cinemaMap['movies'] != null && cinemaMap['movies'] is List) {
+          final moviesDataList = cinemaMap['movies'] as List<dynamic>;
+          moviesList = moviesDataList.map((movieData) {
+            final movieMap = Map<String, dynamic>.from(movieData);
+
+            return Movie(
+              backdrop: movieMap['backdrop'],
+              cast: [], // You might want to map the cast data as well
+              criticsReview: movieMap['criticsReview'],
+              genre: List<String>.from(movieMap['genre']),
+              id: movieMap['id'],
+              metascoreRating: movieMap['metascoreRating'],
+              numOfRatings: movieMap['numOfRatings'],
+              plot: movieMap['plot'],
+              poster: movieMap['poster'],
+              rating: movieMap['rating'],
+              title: movieMap['title'],
+              year: movieMap['year'],
+            );
+          }).toList();
+        }
+
         return Cinema(
           image: cinemaMap['image'],
           title: cinemaMap['title'],
@@ -27,8 +53,12 @@ class CinemaProvider with ChangeNotifier {
             cinemaMap['location']['longitude'],
           ),
           rating: cinemaMap['rating'],
+          movies: moviesList,
         );
       }).toList();
+
+      // Use addAll to add all mapped cinemas to the existing list
+      cinemasData.addAll(tempCinemasData);
     } else {
       print("No data available from Firebase.");
     }
